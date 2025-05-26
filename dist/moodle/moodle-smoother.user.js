00c58d17ef226ae0c448c50a6f015a347c833768
hachiman-oct <176100494+hachiman-oct@users.noreply.github.com>
2025-05-26 10:49:45 +0900

    update

--- dist/moodle/moodle-smoother.user.js
+++ dist/moodle/moodle-smoother.user.js
@@ -1,4 +1,21 @@
-@file:./moodle/meta.js
+// ==UserScript==
+// @name         Moodle Smoother
+// @namespace    https://github.com/hachiman-oct/
+// @version      1.1.0
+// @description  Refactored version with modular settings and function mapping
+// @match        https://wsdmoodle.waseda.jp/*
+// @icon         https://raw.githubusercontent.com/hachiman-oct/waseda-userscripts/main/moodle/moodle-smoother-icon.svg
+// @updateURL    https://raw.githubusercontent.com/hachiman-oct/waseda-userscripts/main/moodle/moodle-smoother.user.js
+// @downloadURL  https://raw.githubusercontent.com/hachiman-oct/waseda-userscripts/main/moodle/moodle-smoother.user.js
+// @license      MIT
+// @resource     externalCSS https://raw.githubusercontent.com/hachiman-oct/waseda-userscripts/main/moodle/style.css
+// @grant        GM_getResourceText
+// @resource     moodleLogo https://moodle.org/theme/moodleorg/pix/moodle_logo_TM.svg
+// @grant        GM_getResourceURL
+// @grant        GM_setValue
+// @grant        GM_getValue
+// ==/UserScript==
+
 (() => {
   // moodle/features/changeHeader.js
   function changeHeader(qs, GM_getResourceURL2) {
@@ -371,4 +388,3 @@
     }
   })();
 })();
-// @license      MIT
